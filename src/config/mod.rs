238413841303678947b6e6b7b209bad8c773383a
CJ Cummings <cj@formata.io>
2025-02-28 15:02:14 -0700

    start to users api

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -14,7 +14,7 @@
 // limitations under the License.
 //
 
-use stof::SDoc;
+use stof::{SDoc, SVal};
 
 
 /// Stof Types for Config file.
@@ -48,6 +48,15 @@ type Registry {
     path: str = 'registry';
 }
 
+// By default, the runner is unprotected
+type Admin {
+    username: str = 'admin'
+    password: str = ''
+
+    // permissions granted to any unauthenticated user
+    unauth_perms: int = 0b000;
+}
+
 type Runner {
     #[schema]
     server: Server = new Server {};
@@ -55,11 +64,18 @@ type Runner {
     #[schema]
     registry: Registry = new Registry {};
 
+    #[schema]
+    admin: Admin = new Admin {};
+
     #[run]
     fn run() {
         self.valid = self.schemafy(self);
     }
 }
+
+fn unauth_read(): bool { return root.admin.unauth_perms & 0b001 > 0; }
+fn unauth_modify(): bool { return root.admin.unauth_perms & 0b010 > 0; }
+fn unauth_exec(): bool { return root.admin.unauth_perms & 0b100 > 0; }
 
 #[init]
 fn init() {
@@ -96,4 +112,185 @@ pub fn load_config(file: Option<String>) -> Result<SDoc, String> {
     }
 
     Ok(doc)
+}
+
+
+/// Users file name.
+const USERS_FILE_NAME: &str = "__users__.json";
+const USERS_INTERFACE: &str = r#"
+// make sure the Users root exists
+root Users: {}
+
+type User {
+    username: str;
+
+    #[private]
+    password: str;
+
+    // yes or no to: execute stof, modify registry, read registry
+    // 0 is no permissions or the same as unauthenticated
+    perms: int = 0b000;
+
+    // modify scope
+    // if set, this user can only modify this registry within this scope
+    scope: str = '';
+
+    fn authenticated(password: str): bool {
+        return self.password == password;
+    }
+    fn can_read_registry(): bool {
+        return self.perms & 0b001 > 0;
+    }
+    fn can_modify_registry(): bool {
+        return self.perms & 0b010 > 0;
+    }
+    fn can_modify_scope(path: str): bool {
+        let user_scope = self.scope;
+        if (user_scope.len() < 1) return true;
+
+        let scope = path.split('/').first();
+        if (scope.startsWith('@')) scope = scope.substring(1);
+        return scope == user_scope;
+    }
+    fn can_exec(): bool {
+        return self.perms & 0b100 > 0;
+    }
+}
+
+obj Admin: {
+    // set a user
+    fn set_user(username: str, password: str, perms: int = 0b111, scope: str = ''): bool {
+        Users.removeField(username, true);
+        return Users.set(username, new User {
+            username: username,
+            password: password,
+            perms: perms,
+            scope: scope,
+        });
+    }
+
+    // delete a user by username
+    fn delete_user(username: str): bool {
+        return Users.removeField(username, true);
+    }
+
+    // export users to a json file
+    fn export_json_users(path: str) {
+        let json = stringify(Users, 'json');
+        fs.write(path, json);
+    }
+}
+
+// authenticate a user by username, returning the user if present
+fn authenticate(username: str, password: str): User {
+    let user: User = Users.at(username);
+    if (user && user.authenticated(password)) {
+        return user;
+    }
+    return null;
+}
+
+// can this user read the registry?
+fn can_read_registry(username: str, password: str): bool {
+    let user = self.authenticate(username, password);
+    return user && user.can_read_registry();
+}
+
+// can this user modify the registry?
+fn can_modify_registry(username: str, password: str, path: str = ''): bool {
+    let user = self.authenticate(username, password);
+    return user && user.can_modify_registry() && (path.len() < 1 || user.can_modify_scope(path));
+}
+
+// can this user exec on the server?
+fn can_exec(username: str, password: str): bool {
+    let user = self.authenticate(username, password);
+    return user && user.can_exec();
+}
+"#;
+
+
+/// Get admin (if defined).
+/// Returns admin username & password if the configuration contains an admin definition (both username and non-empty password).
+pub(crate) fn get_admin(config: &SDoc) -> Option<(String, String)> {
+    if let Some(username) = config.field("root.admin.username", None) {
+        if let Some(password) = config.field("root.admin.password", None) {
+            let user = username.to_string();
+            let pass = password.to_string();
+            if user.len() > 0 && pass.len() > 0 {
+                return Some((user, pass));
+            }
+        }
+    }
+    None
+}
+
+
+/// Load the stof users document.
+pub(crate) fn load_users(config: &SDoc) -> (String, SDoc) {
+    let mut registry_path = String::from("registry");
+    if let Some(field) = config.field("root.registry.path", None) {
+        registry_path = field.to_string();
+    }
+    let users_file_path = format!("{}/{}", registry_path, USERS_FILE_NAME);
+
+    let mut doc = SDoc::default();
+    let _ = doc.file_import("main", "json", &users_file_path, "json", "Users");
+    let _ = doc.string_import("main", "stof", USERS_INTERFACE, "");
+
+    (users_file_path, doc)
+}
+
+
+/// ADMIN save users.
+pub(crate) fn admin_export_users(pair: &mut (String, SDoc)) {
+    let path = SVal::from(&pair.0);
+    let _ = pair.1.call_func("root.Admin.export_json_users", None, vec![path]);
+}
+
+
+/// ADMIN create a new user.
+pub(crate) fn admin_set_user(pair: &mut (String, SDoc), user: &str, pass: &str, perms: i64, scope: &str) -> bool {
+    if let Ok(res) = pair.1.call_func("root.Admin.set_user", None, vec![user.into(), pass.into(), perms.into(), scope.into()]) {
+        admin_export_users(pair);
+        return res.truthy();
+    }
+    false
+}
+
+
+/// ADMIN delete a user.
+pub(crate) fn admin_delete_user(pair: &mut (String, SDoc), user: &str) -> bool {
+    if let Ok(res) = pair.1.call_func("root.Admin.delete_user", None, vec![user.into()]) {
+        admin_export_users(pair);
+        return res.truthy();
+    }
+    false
+}
+
+
+/// Can read registry?
+pub(crate) fn can_read_registry(pair: &mut (String, SDoc), user: &str, pass: &str) -> bool {
+    if let Ok(res) = pair.1.call_func("root.can_read_registry", None, vec![user.into(), pass.into()]) {
+        return res.truthy();
+    }
+    false
+}
+
+
+/// Can modify registry?
+pub(crate) fn can_modify_registry(pair: &mut (String, SDoc), user: &str, pass: &str, path: &str) -> bool {
+    if let Ok(res) = pair.1.call_func("root.can_modify_registry", None, vec![user.into(), pass.into(), path.into()]) {
+        return res.truthy();
+    }
+    false
+}
+
+
+/// Can exec?
+pub(crate) fn can_exec(pair: &mut (String, SDoc), user: &str, pass: &str) -> bool {
+    if let Ok(res) = pair.1.call_func("root.can_exec", None, vec![user.into(), pass.into()]) {
+        return res.truthy();
+    }
+    false
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -15,6 +15,7 @@
 //
 
 mod response;
+use http_auth_basic::Credentials;
 use response::StofResponse;
 
 mod sandbox_fs;
@@ -22,19 +23,23 @@ use sandbox_fs::TmpFileSystemLibrary;
 use stof_http::HTTPLibrary;
 
 use std::{collections::BTreeMap, net::SocketAddr, sync::Arc, time::Duration};
-use axum::{body::Bytes, extract::{Path, Query, State}, http::{header::CONTENT_TYPE, HeaderMap, StatusCode}, response::IntoResponse, routing::{get, post}, Router};
+use axum::{body::Bytes, extract::{Path, Query, State}, http::{header::{AUTHORIZATION, CONTENT_TYPE}, HeaderMap, StatusCode}, response::IntoResponse, routing::{get, post}, Router};
 use stof::{SDoc, SField, SUnits, SVal};
 use tokio::{sync::Mutex, time::timeout};
 use tower_governor::{governor::GovernorConfig, GovernorLayer};
 use tower_http::cors::CorsLayer;
 
-use crate::registry::{add_registry_pkg, delete_registry_pkg, get_registry_pkg, LPKG};
+use crate::{admin_delete_user, admin_set_user, can_exec, can_modify_registry, can_read_registry, get_admin, load_users, registry::{add_registry_pkg, delete_registry_pkg, get_registry_pkg, LPKG}};
 
 
 /// Server state.
 #[derive(Clone)]
 pub struct ServerState {
+    /// Configuration document.
     pub config: Arc<Mutex<SDoc>>,
+
+    /// (path.bstof, users document).
+    pub users: Arc<Mutex<(String, SDoc)>>,
 }
 impl ServerState {
     /// Return specific stof errors?
@@ -152,16 +157,26 @@ pub async fn run_server(config: SDoc) {
     let cors = CorsLayer::permissive();
 
     // Server state
+    let users = load_users(&config);
     let state = ServerState {
         config: Arc::new(Mutex::new(config)),
+        users: Arc::new(Mutex::new(users)),
     };
 
     // Create the application router
     let app = Router::new()
-        .route("/registry/{*path}", get(get_request_handler)
-            .put(put_request_handler)
-            .delete(delete_request_handler))
-        .route("/run", post(post_request_handler))
+        // Registry API
+        .route("/registry/{*path}", get(get_registry_handler)
+            .put(put_registry_handler)
+            .delete(delete_registry_handler))
+        
+        // Exec API
+        .route("/run", post(exec_handler))
+        
+        // Admin Users API
+        .route("/admin/users", post(admin_set_user_handler)
+            .delete(admin_delete_user_handler))
+        
         .layer(GovernorLayer {
             config: governor_conf
         })
@@ -190,13 +205,52 @@ pub async fn sandbox_document(doc: &mut SDoc, state: ServerState) {
     doc.load_format(Arc::new(LPKG::new(state.registry_path().await)));
 }
 
+
+/*****************************************************************************
+ * Registry API.
+ *****************************************************************************/
+
 /// Put request handler - add a package to the registry.
-async fn put_request_handler(State(state): State<ServerState>, Path(path): Path<String>, Query(query): Query<BTreeMap<String, String>>, _headers: HeaderMap, body: Bytes) -> impl IntoResponse {
+async fn put_registry_handler(State(state): State<ServerState>, Path(path): Path<String>, Query(query): Query<BTreeMap<String, String>>, headers: HeaderMap, body: Bytes) -> impl IntoResponse {
     if !state.registry_enabled().await {
         return StofResponse::error(StatusCode::NOT_IMPLEMENTED, "registry is not enabled");
     }
     if path.split('/').collect::<Vec<&str>>().len() < 2 {
         return StofResponse::error(StatusCode::BAD_REQUEST, "package directory not found");
+    }
+
+    // authorization for modifying this registry
+    // basic "Authorization" header access, with base64 encoding of "user:pass". Ex. "Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ="
+    {
+        let mut config = state.config.lock().await;
+        if let Some(admin) = get_admin(&config) {
+            if let Some(authorization) = headers.get(AUTHORIZATION) {
+                if let Ok(credentials) = Credentials::from_header(authorization.to_str().unwrap().to_string()) {
+                    let user = credentials.user_id;
+                    let pass = credentials.password;
+
+                    if user == admin.0 && pass == admin.1 {
+                        // granted access as admin
+                    } else {
+                        // need to check with users for access
+                        let mut pair = state.users.lock().await;
+                        if !can_modify_registry(&mut pair, &user, &pass, &path) {
+                            return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                        }
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            } else {
+                if let Ok(res) = config.call_func("root.unauth_modify", None, vec![]) {
+                    if !res.truthy() {
+                        return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            }
+        }
     }
 
     let registry_path = state.registry_path().await;
@@ -215,12 +269,46 @@ async fn put_request_handler(State(state): State<ServerState>, Path(path): Path<
 }
 
 /// Delete request handler - remove an interface from the registry.
-async fn delete_request_handler(State(state): State<ServerState>, Path(path): Path<String>, Query(_query): Query<BTreeMap<String, String>>, _headers: HeaderMap, _body: Bytes) -> impl IntoResponse {
+async fn delete_registry_handler(State(state): State<ServerState>, Path(path): Path<String>, Query(_query): Query<BTreeMap<String, String>>, headers: HeaderMap, _body: Bytes) -> impl IntoResponse {
     if !state.registry_enabled().await {
         return StofResponse::error(StatusCode::NOT_IMPLEMENTED, "registry is not enabled");
     }
     if path.split('/').collect::<Vec<&str>>().len() < 2 {
         return StofResponse::error(StatusCode::BAD_REQUEST, "package directory not found");
+    }
+
+    // authorization for modifying this registry
+    // basic "Authorization" header access, with base64 encoding of "user:pass". Ex. "Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ="
+    {
+        let mut config = state.config.lock().await;
+        if let Some(admin) = get_admin(&config) {
+            if let Some(authorization) = headers.get(AUTHORIZATION) {
+                if let Ok(credentials) = Credentials::from_header(authorization.to_str().unwrap().to_string()) {
+                    let user = credentials.user_id;
+                    let pass = credentials.password;
+
+                    if user == admin.0 && pass == admin.1 {
+                        // granted access as admin
+                    } else {
+                        // need to check with users for access
+                        let mut pair = state.users.lock().await;
+                        if !can_modify_registry(&mut pair, &user, &pass, &path) {
+                            return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                        }
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            } else {
+                if let Ok(res) = config.call_func("root.unauth_modify", None, vec![]) {
+                    if !res.truthy() {
+                        return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            }
+        }
     }
 
     let registry_path = state.registry_path().await;
@@ -233,12 +321,46 @@ async fn delete_request_handler(State(state): State<ServerState>, Path(path): Pa
 }
 
 /// Get request handler - get an interface from a registry.
-async fn get_request_handler(State(state): State<ServerState>, Path(path): Path<String>, Query(_query): Query<BTreeMap<String, String>>, _headers: HeaderMap) -> impl IntoResponse {
+async fn get_registry_handler(State(state): State<ServerState>, Path(path): Path<String>, Query(_query): Query<BTreeMap<String, String>>, headers: HeaderMap) -> impl IntoResponse {
     if !state.registry_enabled().await {
         return StofResponse::error(StatusCode::NOT_IMPLEMENTED, "registry is not enabled");
     }
     if path.split('/').collect::<Vec<&str>>().len() < 2 {
         return StofResponse::error(StatusCode::BAD_REQUEST, "package directory not found");
+    }
+
+    // authorization for reading this registry
+    // basic "Authorization" header access, with base64 encoding of "user:pass". Ex. "Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ="
+    {
+        let mut config = state.config.lock().await;
+        if let Some(admin) = get_admin(&config) {
+            if let Some(authorization) = headers.get(AUTHORIZATION) {
+                if let Ok(credentials) = Credentials::from_header(authorization.to_str().unwrap().to_string()) {
+                    let user = credentials.user_id;
+                    let pass = credentials.password;
+
+                    if user == admin.0 && pass == admin.1 {
+                        // granted access as admin
+                    } else {
+                        // need to check with users for access
+                        let mut pair = state.users.lock().await;
+                        if !can_read_registry(&mut pair, &user, &pass) {
+                            return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                        }
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            } else {
+                if let Ok(res) = config.call_func("root.unauth_read", None, vec![]) {
+                    if !res.truthy() {
+                        return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            }
+        }
     }
     
     let registry_path = state.registry_path().await;
@@ -250,11 +372,51 @@ async fn get_request_handler(State(state): State<ServerState>, Path(path): Path<
     StofResponse::error(StatusCode::NOT_FOUND, "package not found")
 }
 
+
+/*****************************************************************************
+ * Exec API.
+ *****************************************************************************/
+
 /// Post request handler - run some stof.
-async fn post_request_handler(State(state): State<ServerState>, Query(query): Query<BTreeMap<String, String>>, headers: HeaderMap, mut body: Bytes) -> impl IntoResponse {
+async fn exec_handler(State(state): State<ServerState>, Query(query): Query<BTreeMap<String, String>>, headers: HeaderMap, mut body: Bytes) -> impl IntoResponse {
     if !state.run_stof().await {
         return StofResponse::error(StatusCode::NOT_IMPLEMENTED, "stof runner is not enabled");
     }
+
+    // authorization for execution
+    // basic "Authorization" header access, with base64 encoding of "user:pass". Ex. "Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ="
+    {
+        let mut config = state.config.lock().await;
+        if let Some(admin) = get_admin(&config) {
+            if let Some(authorization) = headers.get(AUTHORIZATION) {
+                if let Ok(credentials) = Credentials::from_header(authorization.to_str().unwrap().to_string()) {
+                    let user = credentials.user_id;
+                    let pass = credentials.password;
+
+                    if user == admin.0 && pass == admin.1 {
+                        // granted access as admin
+                    } else {
+                        // need to check with users for access
+                        let mut pair = state.users.lock().await;
+                        if !can_exec(&mut pair, &user, &pass) {
+                            return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                        }
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            } else {
+                if let Ok(res) = config.call_func("root.unauth_exec", None, vec![]) {
+                    if !res.truthy() {
+                        return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            }
+        }
+    }
+
     let time = state.run_timeout().await;
     let result = timeout(time, async move {
         let mut content_type = String::from("stof");
@@ -339,4 +501,107 @@ async fn post_request_handler(State(state): State<ServerState>, Query(query): Qu
             StofResponse::error(StatusCode::REQUEST_TIMEOUT, "timeout while running document")
         }
     }
+}
+
+
+/*****************************************************************************
+ * Admin Users API.
+ *****************************************************************************/
+
+/// Create/update a user in this registry.
+async fn admin_set_user_handler(State(state): State<ServerState>, headers: HeaderMap, body: Bytes) -> impl IntoResponse {
+    // authorization for admin API
+    // basic "Authorization" header access, with base64 encoding of "user:pass". Ex. "Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ="
+    {
+        let config = state.config.lock().await;
+        if let Some(admin) = get_admin(&config) {
+            if let Some(authorization) = headers.get(AUTHORIZATION) {
+                if let Ok(credentials) = Credentials::from_header(authorization.to_str().unwrap().to_string()) {
+                    let user = credentials.user_id;
+                    let pass = credentials.password;
+                    if user == admin.0 && pass == admin.1 {
+                        // granted access as admin
+                    } else {
+                        return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            } else {
+                return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+            }
+        } else {
+            return StofResponse::error(StatusCode::NOT_IMPLEMENTED, "not available");
+        }
+    }
+    
+    let mut content_type = String::from("stof");
+    if let Some(ctype) = headers.get(CONTENT_TYPE) {
+        content_type = ctype.to_str().unwrap().to_owned();
+    }
+    if let Ok(doc) = SDoc::bytes(body, &content_type) {
+        if let Some(user) = doc.field("root.username", None) {
+            if let Some(pass) = doc.field("root.password", None) {
+                if let Some(perms) = doc.field("root.perms", None) {
+                    let mut scope = String::default();
+                    if let Some(scope_field) = doc.field("root.scope", None) {
+                        scope = scope_field.to_string();
+                    }
+                    match &perms.value {
+                        SVal::Number(num) => {
+                            let perms = num.int();
+                            let mut pair = state.users.lock().await;
+                            if admin_set_user(&mut pair, &user.to_string(), &pass.to_string(), perms, &scope) {
+                                return StofResponse::msg(StatusCode::OK, "set user");
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+    StofResponse::error(StatusCode::BAD_REQUEST, "not a valid user body")
+}
+
+/// Delete a user in this registry.
+async fn admin_delete_user_handler(State(state): State<ServerState>, headers: HeaderMap, body: Bytes) -> impl IntoResponse {
+    // authorization for admin API
+    // basic "Authorization" header access, with base64 encoding of "user:pass". Ex. "Authorization: Basic dXNlcm5hbWU6cGFzc3dvcmQ="
+    {
+        let config = state.config.lock().await;
+        if let Some(admin) = get_admin(&config) {
+            if let Some(authorization) = headers.get(AUTHORIZATION) {
+                if let Ok(credentials) = Credentials::from_header(authorization.to_str().unwrap().to_string()) {
+                    let user = credentials.user_id;
+                    let pass = credentials.password;
+                    if user == admin.0 && pass == admin.1 {
+                        // granted access as admin
+                    } else {
+                        return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                    }
+                } else {
+                    return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+                }
+            } else {
+                return StofResponse::error(StatusCode::FORBIDDEN, "access denied");
+            }
+        } else {
+            return StofResponse::error(StatusCode::NOT_IMPLEMENTED, "not available");
+        }
+    }
+    
+    let mut content_type = String::from("stof");
+    if let Some(ctype) = headers.get(CONTENT_TYPE) {
+        content_type = ctype.to_str().unwrap().to_owned();
+    }
+    if let Ok(doc) = SDoc::bytes(body, &content_type) {
+        if let Some(user) = doc.field("root.username", None) {
+            let mut pair = state.users.lock().await;
+            if admin_delete_user(&mut pair, &user.to_string()) {
+                return StofResponse::msg(StatusCode::OK, "deleted");
+            }
+        }
+    }
+    StofResponse::error(StatusCode::BAD_REQUEST, "not a valid user body")
 }
